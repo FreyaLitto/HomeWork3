51d34dc9439a5b6e71c9b61e741146bc5db700d0
Shaun <Shaun@SHAUN-PC>
2023-06-18 12:24:07 +0100

    Add project files.

---
+++ HomeWork3/Program.cs
@@ -0,0 +1,33 @@
+﻿int inputNumber = int.Parse(Console.ReadLine());
+if (inputNumber < 2)
+{
+    Console.WriteLine("Is not a Prime Number");
+}
+else if (inputNumber == 2)
+{
+    Console.WriteLine("Is  a Prime Number");
+}
+else if (inputNumber % 2 == 0)
+{
+    Console.WriteLine("Is not a Prime Number");
+}
+else
+{
+    int divisor = 3;
+    bool isPrime = true;
+    while (divisor < inputNumber)
+    {
+        if (inputNumber % divisor == 0)
+        {
+            isPrime = false;
+            Console.WriteLine("Is not a Prime Number");
+            break;
+        }
+        divisor += 2;
+    }
+    if (isPrime)
+    {
+        Console.WriteLine("Is  a Prime Number");
+    }
+}
+
